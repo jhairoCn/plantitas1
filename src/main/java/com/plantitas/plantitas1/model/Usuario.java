0b57bc308853b4ac239b83a2a0cabef0e40a5cee
jhairoCn <jhairocontreras12@gmail.com>
2026-07-05 11:46:43 -0600

    cambios3creo

--- src/main/java/com/plantitas/plantitas1/model/Usuario.java
+++ src/main/java/com/plantitas/plantitas1/model/Usuario.java
@@ -1,27 +1,32 @@
 package com.plantitas1.plantitas1.model;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
-import com.plantitas.plantitas1.model.Usuario;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String nombre;
     private String correo_electronico;    
     private String hash_de_contrasena;    
     private String nivel;
     private String avatar_url;
-    private java.time.LocalDateTime creado_en;
+    private LocalDateTime creado_en;
     
 }
 
--- src/main/java/com/plantitas/plantitas1/service/UsuarioService.java
+++ src/main/java/com/plantitas/plantitas1/service/UsuarioService.java
@@ -1,11 +1,15 @@
 package com.plantitas1.plantitas1.service;
 
+import com.plantitas1.plantitas1.model.Usuario; 
 import com.plantitas1.plantitas1.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+import java.util.List; 
 
+@Service 
 public class UsuarioService {
+    
     private final UsuarioRepository usuarioRepository;
 
-    
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
@@ -28,6 +32,5 @@ public class UsuarioService {
 
     public void deleteUsuario(Long id){
         usuarioRepository.deleteById(id);
-    }
-
+        }
 }
