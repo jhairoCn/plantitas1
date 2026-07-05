fc6aaf35aa061c50a3fcbc18231a8cf0c95f4690
jhairoCn <jhairocontreras12@gmail.com>
2026-07-05 11:35:37 -0600

    cambios2

--- src/main/java/com/plantitas/plantitas1/repository/UsuarioRepository.java
+++ src/main/java/com/plantitas/plantitas1/repository/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.plantitas1.plantitas1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.plantitas1.plantitas1.repository.UsuarioRepository;
 import com.plantitas1.plantitas1.model.Usuario;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 }
